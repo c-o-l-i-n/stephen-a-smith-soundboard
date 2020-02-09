232df747b0a33016a9d4fcbbbe6fc2fe4d2000fa
Colin Williams <williamsca20@gmail.com>
2020-02-08 21:59:07 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('no-no-no-no-no-btn').addEventListener('click', () => {
+        document.getElementById('no-no-no-no-no').play(); 
+    });    
+});
+
