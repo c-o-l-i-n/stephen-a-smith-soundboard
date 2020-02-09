ad5240f348b571175160f990a58194ff5735a89d
Colin Williams <williamsca20@gmail.com>
2020-02-09 00:23:38 -0500

    Some stuff

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 let sounds = [
     'no-no-no-no-no',
+    'what$$-Me$$',
     'consider-having-them-arrested',
+    'this-is-bad',
     'very-bad-day',
     'one-of-the-worst-days',
     'suspended-with-pay',
     'blasphemy',
-    'how-do-you-do-that',
-    'what-the-hell-is-going-on',
-    'heh-heh-heh-heh,-what-do-you-think',
-    'what-the-hell-is-goin-on-i-have-no-idea',
+    'how-do-you-do-that$',
+    'what-the-hell-is-going-on$',
+    'heh-heh-heh-heh,-what-do-you-think$',
+    'what-the-hell-is-goin-on-I-have-no-idea',
     'i-have-no-idea',
     'oh-no-you-didn\'t',
     'we\'re-in-a-different-era',
-    'this-is-bad',
     'we-have-been-hoodwinked',
     'y\'all-take-it-easy,-I\'m-out',
     'i-am-in-a-very-very-very-good-mood'
@@ -32,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function addSound(containerDiv, soundName) {
-    var prettyName = soundName.replace(/-/g, ' ');
+    var prettyName = soundName.replace(/-/g, ' ').replace(/\$/g, '?');
     prettyName =  prettyName.charAt(0).toUpperCase() + prettyName.slice(1);
     containerDiv.innerHTML += `
         <audio id="` + soundName + `" src="assets/sounds/` + soundName + `.mp3" preload="metadata" type="audio/mpeg">
@@ -40,4 +41,24 @@ function addSound(containerDiv, soundName) {
         </audio>
         <button id="` + soundName + `-btn" class="button is-danger">` + prettyName + `</button>
         `;
+    // var audio = document.getElementById(soundName);
+    // var button = document.getElementById(soundName + '-btn');
+    // audio.addEventListener('canplay', () => {
+    //     button.classList.remove('is-loading');
+    // });
+    // button.addEventListener('click', () => {
+    //     audio.play();
+    // });
 }
+
+// function onCanPlay(soundName) {
+//     console.log('poop');
+//     var audio = document.getElementById(soundName);
+//     var button = document.getElementById(soundName + '-btn');
+//     audio.addEventListener('canplay', () => {
+//         button.classList.remove('is-loading');
+//     });
+//     button.addEventListener('click', () => {
+//         audio.play();
+//     });
+// }
