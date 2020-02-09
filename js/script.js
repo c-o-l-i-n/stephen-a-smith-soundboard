09bd756423787837783d862155c5398bad2f4b42
Colin Williams <williamsca20@gmail.com>
2020-02-08 22:39:29 -0500

    Added 2 sounds

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById('no-no-no-no-no-btn').addEventListener('click', () => {
-        document.getElementById('no-no-no-no-no').play(); 
-    });    
+let sounds = [
+    'no-no-no-no-no',
+    'consider-having-them-arrested',
+    'very-bad-day'
+]
+
+document.addEventListener("DOMContentLoaded", () => {
+    let soundsDiv = document.getElementById('sounds');
+    
+    sounds.forEach(el => {
+        addSound(soundsDiv, el)
+        document.getElementById(el + '-btn').addEventListener('click', () => {
+            document.getElementById(el).play();
+        });
+    });   
 });
 
+function addSound(containerDiv, soundName) {
+    var prettyName = soundName.replace(/-/g, ' ');
+    prettyName =  prettyName.charAt(0).toUpperCase() + prettyName.slice(1);
+    containerDiv.innerHTML += `
+        <audio id="` + soundName + `" src="assets/sounds/` + soundName + `.mp3" preload="metadata" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+        <button id="` + soundName + `-btn" class="button is-danger">` + prettyName + `</button>
+        `;
+}
